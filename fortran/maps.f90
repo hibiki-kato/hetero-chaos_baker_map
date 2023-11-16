020f10fc3b1ff34c7224f378e5d9e0aabf9cfd81
hibiki-kato <katohibiki17@gmail.com>
2023-11-16 10:57:25 +0000

    update maps

--- fortran/maps.f90
+++ fortran/maps.f90
@@ -3,40 +3,47 @@ module maps
 
 contains
    subroutine bakers_map(x, y)
-         real(8), intent(inout) :: x, y
-         if (x < 0.5) then
-               x = 2.0 * x
-               y = 0.5 * y
-         else
-               x = 2.0 * x - 1
-               y = 0.5 + 0.5 * y
-         end if
+      real(8), intent(inout) :: x, y
+      if (x < 0.5) then
+            x = 2.0 * x
+            y = 0.5 * y
+      else
+            x = 2.0 * x - 1
+            y = 0.5 + 0.5 * y
+      end if
    end subroutine bakers_map
 
    subroutine rolling_bakers_map(x, y)
-         real(8), intent(inout) :: x, y
-         if (x < 0.5) then
-               x = 2.0 * x
-               y = 0.5 * y
-         else
-               x = 2.0 - 2.0 * x
-               y = 1.0 - 0.5 * y
-         end if
+      real(8), intent(inout) :: x, y
+      if (x < 0.5) then
+            x = 2.0 * x
+            y = 0.5 * y
+      else
+            x = 2.0 - 2.0 * x
+            y = 1.0 - 0.5 * y
+      end if
    end subroutine rolling_bakers_map
 
+   subroutine 3Dhetero-chaos_bakers_map(x, y, z, a, b)
+      real(8), intent(inout) :: x, y, z
+      real(8), intent(in) :: a, b
+      
+
+   end subroutine 3Dhetero-chaos_bakers_map
+
    subroutine logistic_map(x, a_in)
-         real(8), intent(inout) :: x
-         real(8), intent(in) :: a_in
-         
-         x = a_in * x * (1.0 - x)
+      real(8), intent(inout) :: x
+      real(8), intent(in) :: a_in
+      
+      x = a_in * x * (1.0 - x)
    end subroutine logistic_map
 
    subroutine coupled_logistic_map(x, y, a, b, c, d)
-         real(8), intent(inout) :: x, y
-         real(8), intent(in) :: a, b, c, d
+      real(8), intent(inout) :: x, y
+      real(8), intent(in) :: a, b, c, d
 
-         x = a * x * (1.0 - x) + b * y
-         y = c * y * (1.0 - y) + d * x
+      x = a * x * (1.0 - x) + b * y
+      y = c * y * (1.0 - y) + d * x
    end subroutine coupled_logistic_map
-
+      
 end module maps
